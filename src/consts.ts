f52c0274c11abf6e234f69298c93d0b0ff764d79
andersseen <andriipap01@gmail.com>
2026-03-15 14:45:18 +0100

    fix: correct spelling of "Andrii Pap" in various files

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Andrii Pa Blog";
+export const SITE_TITLE = "Andrii Pap Blog";
 export const SITE_DESCRIPTION =
-  "Articulos sobre frontend, arquitectura web y design systems por Andrii Pa (@Andersseen).";
+  "Articulos sobre frontend, arquitectura web y design systems por Andrii Pap (@Andersseen).";
 
 export const SITE_URL = "https://www.andriipap.dev";
-export const AUTHOR_NAME = "Andrii Pa";
+export const AUTHOR_NAME = "Andrii Pap";
 export const AUTHOR_USERNAME = "Andersseen";
 export const AUTHOR_MEDIUM_URL = "https://medium.com/@andriipap";
 export const AUTHOR_PORTFOLIO_URL = "https://www.andriipap.dev/";
