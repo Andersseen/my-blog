915de537a8c91b149196956a373c260466cb8407
andersseen <andriipap01@gmail.com>
2026-06-07 12:47:22 +0200

    feat: update web

--- pagefind.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://andersseen.dev",
-  integrations: [],
-  // Pagefind will index the dist folder after build
-  // Run: npx pagefind --site dist
-});
